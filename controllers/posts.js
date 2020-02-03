40a78ad3fc84d10111fad061802e72a14d785a4e
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-02-03 15:42:25 -0800

    Users can submit and save posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,19 @@
-module.exports = app => {
+const Post = require('../models/post');
+
+module.exports = (app) => {
+
     // CREATE
-    app.post("/posts/new", (req, res) => {
-        console.log(req.body);
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            // REDIRECT TO THE ROOT
+            console.log(err)
+            console.log(post)
+            return res.redirect(`/`);
+        })
     });
+
 };
--- server.js
+++ server.js
@@ -4,27 +4,31 @@ var exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 
-// Use Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+var app = express();
 
 // Add after body parser initialization!
 app.use(expressValidator());
 
-// Middle Ware
-var app = express();
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
 require("./controllers/posts.js")(app);
+// Set db
+require("./data/reddit-db");
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/posts/new", function (rew, res) {
+app.get("/posts/new", function(rew, res) {
   res.render("posts-new");
 });
 
