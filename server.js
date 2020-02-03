d00ad2511f17107f9d5d393d06c2a02c890637ef
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-02-02 22:05:21 -0800

    comments

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
+var express = require("express");
+var exphbs = require("express-handlebars");
 
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Middle Ware
 var app = express();
-require('./controllers/posts.js')(app);
+require("./controllers/posts.js")(app);
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/posts/new', function (rew, res) {
-    res.render('posts-new');
-})
+app.get("/posts/new", function (rew, res) {
+  res.render("posts-new");
+});
 
 app.listen(3000);
