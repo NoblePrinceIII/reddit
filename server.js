b5f066821e12893a1a7d93dfda7423b340a89a13
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-02-08 13:57:05 -0800

    Hashed Password Key (I think)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require('dotenv').config();
 
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
@@ -29,6 +30,7 @@ app.get("/posts/new", (req, res) => res.render("posts-new"));
 //Controllers
 require("./controllers/posts")(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
