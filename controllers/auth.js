e3b951d96647ff01ccf6fd0137a8c51d5f712c7a
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-02-09 12:34:54 -0800

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,7 @@
 const User = require("../models/user");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-
-
-module.exports = (app) => {
+module.exports = app => {
     // SIGN UP FORM
     app.get("/sign-up", (req, res) => {
         res.render("sign-up");
@@ -13,17 +11,21 @@ module.exports = (app) => {
         // Create User and JWT
         const user = new User(req.body);
 
-        user.save().then((user) => {
+        user
+            .save()
+            .then(user => {
                 var token = jwt.sign({
-                    _id: user._id
-                }, process.env.SECRET, {
-                    expiresIn: "60 days"
-                });
-                res.cookie('nToken', token, {
+                        _id: user._id
+                    },
+                    process.env.SECRET, {
+                        expiresIn: "60 days"
+                    }
+                );
+                res.cookie("nToken", token, {
                     maxAge: 900000,
                     httpOnly: true
                 });
-                res.redirect('/');
+                res.redirect("/");
             })
             .catch(err => {
                 console.log(err.message);
@@ -33,8 +35,54 @@ module.exports = (app) => {
             });
     });
     // LOGOUT
-    app.get('/logout', (req, res) => {
-        res.clearCookie('nToken');
-        res.redirect('/');
+    app.get("/logout", (req, res) => {
+        res.clearCookie("nToken");
+        res.redirect("/");
     });
-}
+    // LOGIN FORM
+    app.get('/login', (req, res) => {
+        res.render('login');
+    });
+    // LOGIN
+    app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({
+                username
+            }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({
+                        message: "Wrong Username or Password"
+                    });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // Password does not match
+                        return res.status(401).send({
+                            message: "Wrong Username or password"
+                        });
+                    }
+                    // Create a token
+                    const token = jwt.sign({
+                        _id: user._id,
+                        username: user.username
+                    }, process.env.SECRET, {
+                        expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, {
+                        maxAge: 900000,
+                        httpOnly: true
+                    });
+                    res.redirect("/");
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+};
